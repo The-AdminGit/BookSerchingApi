ee3cacaa039f299f24a58921541c25663d1dd727
vivek kumar sah <viveksah4ui@gmail.com>
2024-03-29 02:01:19 +0530

    create(post) book api

--- src/main/java/com/book/restapi/learnbookrestapi/controllers/BookController.java
+++ src/main/java/com/book/restapi/learnbookrestapi/controllers/BookController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.restapi.learnbookrestapi.entities.Book;
@@ -15,12 +17,18 @@ public class BookController {
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/book")
+    @GetMapping("/books")
     public List<Book> getBooks() {
      return this.bookService.getAllBooks();
     }
-    @GetMapping("/book/{id}")
+    @GetMapping("/books/{id}")
     public Book getBookById(@PathVariable int id){
         return this.bookService.getBookById(id);
+    }
+
+    @PostMapping("/books")
+    public Book createBook(@RequestBody Book book){
+        Book b=this.bookService.addBook(book);
+        return b;
     }
 }
